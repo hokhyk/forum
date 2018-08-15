cf9e5bf8d1c4d9f33e4043a72a1f14fe269e8e95
Artemis Fowl LP-Deepin <perudragon@hotmail.com>
2018-08-15 18:35:36 -0500

    02 Models and Rel: channel, discussion, reply

---
+++ app/Discussion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discussion extends Model
+{
+    protected $fillable = [
+
+        'title',
+        'contenido',
+        'user_id',
+        'channel_id',
+    ];
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+}
