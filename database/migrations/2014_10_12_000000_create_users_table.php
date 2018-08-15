28f6badba5fc409474f1abe14086c0e19e3c62c4
Artemis Fowl LP-Deepin <perudragon@hotmail.com>
2018-08-15 18:47:12 -0500

    03 Admin Seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('avatar');
+            $table->boolean('admin')->default(0);
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            // Admin seeder
+            'name' => 'admin',
+            'password' => bcrypt('123456'),
+            'email' => 'm@ya.com',
+            'admin' =>1,
+            'avatar' => asset('avatars/avatar.png')
+        ]);
+    }
+}
